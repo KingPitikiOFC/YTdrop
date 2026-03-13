8c0f5d9115fdbad9e918adf12d6135aec287cb3f
KingPitikiOFC <camiyt2909@gmail.com>
2026-03-12 21:44:11 -0400

    Actualizar run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,40 @@
+#!/usr/bin/env bash
 
+# colors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[0;34m"
+NC="\033[0m" # no color
+
+echo -e "${BLUE}Checking dependencies...${NC}"
+
+# python
+if command -v python >/dev/null 2>&1; then
+    echo -e "${GREEN}python OK${NC}"
+else
+    echo -e "${YELLOW}Installing python${NC}"
+    pkg install python -y
+fi
+
+# ffmpeg
+if command -v ffmpeg >/dev/null 2>&1; then
+    echo -e "${GREEN}ffmpeg OK${NC}"
+else
+    echo -e "${YELLOW}Installing ffmpeg${NC}"
+    pkg install ffmpeg -y
+fi
+
+# yt-dlp
+python - <<EOF
+try:
+    import yt_dlp
+    print("\033[0;32myt_dlp OK\033[0m")
+except ImportError:
+    import os
+    print("\033[1;33mInstalling yt_dlp\033[0m")
+    os.system("python -m pip install yt-dlp")
+EOF
+
+echo -e "${BLUE}Running script...${NC}"
+python yt.py
