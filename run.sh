737e50e5c3093cbf6d37fbe60266357f73965e4e
KingPitikiOFC <camiyt2909@gmail.com>
2026-03-12 22:00:14 -0400

    Actualizar run.sh

--- run.sh
+++ run.sh
@@ -1,45 +1,27 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-=======
 
->>>>>>> e061a51473bacb184aee868efb3541eaa864a9ac
 RED="\033[0;31m"
 GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
-<<<<<<< HEAD
-vBLUE="\033[0;34m"NC="\033[0m"
-=======
 BLUE="\033[0;34m"
 NC="\033[0m"
->>>>>>> e061a51473bacb184aee868efb3541eaa864a9ac
 
 echo -e "${BLUE}Checking dependencies...${NC}"
-<<<<<<< HEAD
-=======
 
->>>>>>> e061a51473bacb184aee868efb3541eaa864a9ac
 if command -v python >/dev/null 2>&1; then
     echo -e "${GREEN}python OK${NC}"
 else
     echo -e "${YELLOW}Installing python${NC}"
-<<<<<<< HEAD
-    pkg install python -yfi
-=======
     pkg install python -y
 fi
 
->>>>>>> e061a51473bacb184aee868efb3541eaa864a9ac
 if command -v ffmpeg >/dev/null 2>&1; then
     echo -e "${GREEN}ffmpeg OK${NC}"
 else
     echo -e "${YELLOW}Installing ffmpeg${NC}"
-<<<<<<< HEAD
-    pkg install ffmpeg -yfi
-=======
     pkg install ffmpeg -y
 fi
 
->>>>>>> e061a51473bacb184aee868efb3541eaa864a9ac
 python - <<EOF
 try:
     import yt_dlp
